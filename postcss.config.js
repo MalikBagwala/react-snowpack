eb01799de4e5d45426a68ac8f44de75fb9b5d7f8
MalikBagwala <malikeditz@gmail.com>
2020-10-26 16:25:00 +0530

    updated tailwind css

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,15 @@ module.exports = {
   plugins: [
     // ...
     require('tailwindcss'),
+    require('@fullhuman/postcss-purgecss')({
+      content: [
+        './src/**/*.js',
+        './src/**/*.jsx',
+        './src/**/*.ts',
+        './src/**/*.tsx',
+      ],
+      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+    }),
     // ...
   ],
 };
