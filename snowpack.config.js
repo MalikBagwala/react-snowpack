ae177b5a9e033c0b43f904270d42c9fe8e110a63
MalikBagwala <malikeditz@gmail.com>
2020-10-26 14:15:02 +0530

    Added Webpack Plugin

--- snowpack.config.js
+++ snowpack.config.js
@@ -4,6 +4,7 @@ module.exports = {
     src: '/_dist_',
   },
   plugins: [
+    '@snowpack/plugin-webpack',
     '@snowpack/plugin-react-refresh',
     '@snowpack/plugin-dotenv',
     '@snowpack/plugin-typescript',
